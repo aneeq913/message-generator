07b10a46e41af579f43ccffc61a695d28bdf96c0
Aneeq Ali <47188268+aneeq913@users.noreply.github.com>
2021-08-31 14:41:52 +0100

    Completing the script.js file

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const randomDrink = () => {
+const RandomDrink = () => {
     //Drinks Array
-    let drink = ['RedBull', 'Coke', 'Diet Coke', 'Vimto', 'Sprite', 'Dr Pepper'];
+    let drinks = ['RedBull', 'Coke', 'Diet Coke', 'Vimto', 'Sprite', 'Dr Pepper'];
     //Numbers array from 1 - 5
     let numbers = [1,2,3,4,5];
+
+    //Randomise the Drinks and Numbers
+    let randomDrink = Math.floor(Math.random() * drinks.length);
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+
+    //Storing the random drink and number in a new array called output
+    let output = [];
+    output.push('You will drink ' + numbers[randomNumber] + ' cans of ' + drinks[randomDrink]);
+    return output;
 };
+
+console.log(RandomDrink());
