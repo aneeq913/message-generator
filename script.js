1e2a99d72e893fe7960ab97cdb7aa132eb8ec7d3
Aneeq Ali <47188268+aneeq913@users.noreply.github.com>
2021-08-31 14:12:59 +0100

    Adding drink and numbers array to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const randomDrink = () => {
+    //Drinks Array
+    let drink = ['RedBull', 'Coke', 'Diet Coke', 'Vimto', 'Sprite', 'Dr Pepper'];
+    //Numbers array from 1 - 5
+    let numbers = [1,2,3,4,5];
+};
